9df2aa8df087c52fe4bd8ee7c7934cdee74c6a61
sacheex <sachithdhanushka00@gmail.com>
2025-01-12 20:13:27 +0530

    Add published_date and registered_date fields to courses and students_courses tables

--- learnhub-database.sql
+++ learnhub-database.sql
@@ -72,6 +72,7 @@ CREATE TABLE IF NOT EXISTS courses (
     price decimal(10,2) NOT NULL,
     pricing_period VARCHAR(50) NOT NULL,
     location VARCHAR(50) NOT NULL,
+    published_date DATE NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY(course_id),
     FOREIGN KEY (subject_id) REFERENCES subjects(subject_id) ON DELETE CASCADE,
     FOREIGN KEY (grade_id) REFERENCES grades(grade_id) ON DELETE CASCADE,
@@ -130,6 +131,7 @@ CREATE TABLE IF NOT EXISTS course_module_date_resources (
 CREATE TABLE IF NOT EXISTS students_courses (
     student_id BIGINT(20) UNSIGNED NOT NULL,
     course_id BIGINT(20) UNSIGNED NOT NULL,
+    registered_date DATE DEFAULT CURRENT_DATE(),
     PRIMARY KEY(student_id, course_id),
     FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
