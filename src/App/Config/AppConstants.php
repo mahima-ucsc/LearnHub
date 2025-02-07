57b9fb6228c11206213a6816aecdaa62914e455f
sacheex <sachithdhanushka00@gmail.com>
2025-02-07 17:45:37 +0530

    Default

--- src/App/Config/AppConstants.php
+++ src/App/Config/AppConstants.php
@@ -9,8 +9,8 @@ class AppConstants
     public const APP_ENV = 'development';
     public const DB_DRIVER = 'mysql';
     public const DB_HOST = 'localhost';
-    public const DB_PORT = 3306;
+    public const DB_PORT = 3312;
     public const DB_NAME = 'learnhub';
-    public const DB_USER = 'learnhub';
-    public const DB_PASS = 'learnhub123';
+    public const DB_USER = 'root';
+    public const DB_PASS = '';
 }
