8c0fe1458a22fda0f2f9398c5af5d7dc2a304513
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2025-02-11 13:47:29 +0530

    Fix SQL seed data by adding a missing comma for user entries

--- seed/seed-users.sql
+++ seed/seed-users.sql
@@ -6,7 +6,7 @@ VALUES
 ('Mahima', 'de Silva', 'mahima@mail.com', '2002-08-18', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'student') ,
 ('Ravindu', 'Manupasan', 'ravindu@mail.com', '2001-05-12', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'student'),
 ('Sachith', 'Abeyrathna', 'sachith@mail.com', '2000-11-23', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'student'),
-('Dinuka', 'Sahan', 'dinuka@mail.com', '1999-07-30', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'student');
+('Dinuka', 'Sahan', 'dinuka@mail.com', '1999-07-30', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'student'),
 -- teachers --
 ('Ajantha', 'Perera', 'ajantha@mail.com', '1980-03-15', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'teacher'),
 ('Chamath', 'Fernando', 'chamath@mail.com', '1975-06-22', NOW(), '$2y$10$YnHCYd20zigSmfBJ2395UurP5X6Ep1vpDhOPM/B3OloMK53syjRDK', 'teacher'),
