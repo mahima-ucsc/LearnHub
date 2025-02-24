a1de5d77635c4670f49d798324efcaf90ee79465
sacheex <sachithdhanushka00@gmail.com>
2025-02-25 02:26:59 +0530

    Enhance loading animation

--- src/App/views/course/Courses.php
+++ src/App/views/course/Courses.php
@@ -17,6 +17,73 @@
         /* Initially hidden */
         z-index: 9999;
     }
+
+    /* From Uiverse.io by 00Kubi */
+    .loader-anime {
+        width: 48px;
+        height: 48px;
+        margin: auto;
+        position: relative;
+    }
+
+    .loader-anime:before {
+        content: '';
+        width: 48px;
+        height: 5px;
+        background: #FFD700;
+        position: absolute;
+        top: 60px;
+        left: 0;
+        border-radius: 50%;
+        animation: shadow324 0.5s linear infinite;
+    }
+
+    .loader-anime:after {
+        content: '';
+        width: 100%;
+        height: 100%;
+        background: #FFD700;
+        position: absolute;
+        top: 0;
+        left: 0;
+        border-radius: 50%;
+        animation: jump7456 0.5s linear infinite;
+    }
+
+    @keyframes jump7456 {
+        15% {
+            border-bottom-right-radius: 50%;
+        }
+
+        25% {
+            transform: translateY(9px) rotate(22.5deg);
+        }
+
+        50% {
+            transform: translateY(18px) scale(1, .9) rotate(45deg);
+            border-bottom-right-radius: 40px;
+        }
+
+        75% {
+            transform: translateY(9px) rotate(67.5deg);
+        }
+
+        100% {
+            transform: translateY(0) rotate(90deg);
+        }
+    }
+
+    @keyframes shadow324 {
+
+        0%,
+        100% {
+            transform: scale(1, 1);
+        }
+
+        50% {
+            transform: scale(1.2, 1);
+        }
+    }
 </style>
 
 <head>
@@ -38,7 +105,11 @@
             </select>
         </div>
         <div class="course-result-container">
-            <div id="loader">Searching...</div>
+            <div id="loader">
+                <!-- From Uiverse.io by 00Kubi -->
+                <div class="loader-anime"></div>
+
+            </div>
             <div class="left-course-container">
                 <div class="filter-section">
                     <div class="filters">
