224ecaa6870b9c22bea590896305574bb938fa44
sacheex <sachithdhanushka00@gmail.com>
2025-03-12 19:12:23 +0530

    Refactor CourseRequestService

--- src/App/Services/CourseRequestService.php
+++ src/App/Services/CourseRequestService.php
@@ -17,13 +17,27 @@ class CourseRequestService
             "INSERT INTO course_requests(title, description, subject_id, user_id)
             VALUES (:title, :description, :subject_id, :user_id)",
             [
-                "title" => $formData['requestTitle'],
-                "description" => $formData['requestDescription'],
+                "title" => $formData['title'],
+                "description" => $formData['description'],
                 "subject_id" => $formData['subject_id'] != -1 ? $formData['subject_id'] : null,
                 "user_id" => $user_id,
             ]
         );
     }
+    // public function create(array $formData)
+    // {
+    //     $user_id = $_SESSION['user'];
+    //     $this->db->query(
+    //         "INSERT INTO course_requests(title, description, subject_id, user_id)
+    //         VALUES (:title, :description, :subject_id, :user_id)",
+    //         [
+    //             "title" => $formData['requestTitle'],
+    //             "description" => $formData['requestDescription'],
+    //             "subject_id" => $formData['subject_id'] != -1 ? $formData['subject_id'] : null,
+    //             "user_id" => $user_id,
+    //         ]
+    //     );
+    // }
 
     public function getCourseRequestsforView()
     {
