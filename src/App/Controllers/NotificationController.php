82e5173fb227ac78e46c490c5f91d64867df72cb
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2025-04-17 11:12:58 +0530

    Implement NotificationController and NotificationService.

---
+++ src/App/Controllers/NotificationController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Services\NotificationService;
+use Framework\TemplateEngine;
+
+class NotificationController
+{
+    public function __construct(
+        private TemplateEngine $view,
+        private NotificationService $notificationService
+    ) {}
+
+    public function getUserNotifications()
+    {
+        $notifications = $this->notificationService->getNotificationsForLoggedInUser();
+        $this->view->renderJson($notifications);
+    }
+}
---
+++ src/App/Services/NotificationService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Framework\Database;
+
+class NotificationService
+{
+    public function __construct(private Database $db) {}
+
+    public function getNotificationsForLoggedInUser()
+    {
+        return $this->db->query(
+            "SELECT message, url, updated_at, is_read FROM notifications n INNER JOIN notification_users n_u ON n.notification_id = n_u.notification_id
+            WHERE n_u.user_id = :user_id
+            ORDER BY n.updated_at DESC",
+            [
+                'user_id' => $_SESSION['user']
+            ]
+        )->findAll();
+    }
+}
