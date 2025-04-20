ab323a02288430dc6ad9622d286b482c5509983d
dinuka2001 <2022cs047@stu.ucsc.cmb.ac.lk>
2025-04-20 23:20:17 +0530

    create eventlistner for togle read and unread button

--- src/App/views/course/course-info/announcements.php
+++ src/App/views/course/course-info/announcements.php
@@ -54,7 +54,8 @@
                                 <!-- <form action="/announcements/mark-as-read" method="POST"> -->
                                 <!-- <input type="hidden" name='announcement_id' value="<?php echo $announcement['announcement_id'] ?>"> -->
                                 <button
-                                    class="<?php echo $announcement['is_read'] == 1 ? "mark-unread-btn" : "mark-read-btn" ?>"
+                                    class="read_btn"
+                                    announcement_id="<?php echo ($announcement['announcement_id']); ?>"
                                     is_read="<?php echo $announcement['is_read'] == 1 ? "true" : "false" ?>">
                                     <?php echo  $announcement['is_read'] == 1 ? "Mark as unread" : "Mark as read" ?>
                                 </button>
@@ -101,6 +102,31 @@
                     checkEmptyState();
                 });
             });
+
+            document.querySelectorAll('.read_btn').forEach(button => {
+                button.addEventListener('click', () => {
+                    const btn = this;
+                    const announcement_id = btn.getAttribute('announcement_id');
+                    const is_read = btn.getAttribute('is_read') === 'true';
+
+                    // update button appearence
+                    btn.textContent = is_read ? "Mark As Unread" : "Mark As Read";
+                    btn.setAttribute('is_read', !is_read);
+
+                    // send AJAX request
+                    fetch('/announcements/mark-as', {
+                            method: 'POST',
+                            headers: {
+                                'content-Type': "application/x-www-form-urlencoded"
+                            },
+                            body: `announcement_id=${announcement_id}&is_read=${!is_read}`
+                        })
+                        .then(response => response.text())
+                        .then(data => {
+                            updateUnreadCount();
+                        })
+                })
+            })
 
             // Mark as read/unread functionality
             let markReadButtons = document.querySelectorAll(".mark-read-btn");
